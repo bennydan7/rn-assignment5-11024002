4c79a472e203210cdad5dedad1181e3e61cd640a
bennydan7 <baowusu-dankwah@st.ug.edu.gh>
2024-06-26 15:41:08 +0000

    modified app

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import * as React from "react";
-import { NavigationContainer, ThemeProvider } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/Ionicons";
 import HomeScreen from "./screens/HomeScreen";
 import MyCards from "./screens/MyCards";
 import Statistics from "./screens/Statistics";
 import Settings from "./screens/Settings";
-import { useContext } from "react";
-import { ThemeContext } from "./ThemeContext";
-import { ThemeLight, ThemeDark } from "./components/appData";
-import Icon from "react-native-vector-icons/Ionicons";
+import { ThemeProvider, ThemeContext } from "./ThemeContext";
+import { ThemeDark, ThemeLight } from "./components/appData";
 
 const Tab = createBottomTabNavigator();
 
-const App = () => {
-  const { theme } = useContext(ThemeContext);
+const AppNavigator = () => {
+  const { theme } = React.useContext(ThemeContext);
 
   const getIconName = (route, focused) => {
     switch (route.name) {
@@ -31,48 +30,53 @@ const App = () => {
   };
 
   return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, size }) => {
+          const iconName = getIconName(route, focused);
+          const iconColor =
+            theme === "dark"
+              ? "#FFFFFF"
+              : focused
+              ? ThemeLight.iconActiveColor
+              : ThemeLight.iconInactiveColor;
+
+          return <Icon name={iconName} size={size} color={iconColor} />;
+        },
+        tabBarActiveTintColor: theme === "dark" ? "#FFFFFF" : "#000000",
+        tabBarInactiveTintColor: theme === "dark" ? "#FFFFFF" : "gray",
+        tabBarStyle: {
+          paddingTop: 15,
+          height: 70,
+          backgroundColor:
+            theme === "dark"
+              ? ThemeDark.backgroundColor
+              : ThemeLight.backgroundColor,
+          borderTopWidth: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        tabBarLabelStyle: {
+          fontWeight: "bold",
+          fontSize: 12,
+          marginBottom: 10,
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="My Cards" component={MyCards} />
+      <Tab.Screen name="Statistics" component={Statistics} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  );
+};
+
+const App = () => {
+  return (
     <ThemeProvider>
       <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            headerShown: false,
-            tabBarIcon: ({ focused, color, size }) => {
-              const iconName = getIconName(route, focused);
-              const iconColor = focused
-                ? theme === "dark"
-                  ? ThemeDark.iconActiveColor
-                  : ThemeLight.iconActiveColor
-                : theme === "dark"
-                ? ThemeDark.iconInactiveColor
-                : ThemeLight.iconInactiveColor;
-
-              return <Icon name={iconName} size={size} color={iconColor} />;
-            },
-            tabBarActiveTintColor: theme === "dark" ? "#383FFA" : "#000000", // Active tab color
-            tabBarInactiveTintColor: "gray", // Inactive tab color
-            tabBarStyle: {
-              paddingTop: 15,
-              height: 70,
-              backgroundColor:
-                theme === "dark"
-                  ? ThemeDark.backgroundColor
-                  : ThemeLight.backgroundColor, // Background color based on theme
-              borderTopWidth: 0, // Remove top border
-              elevation: 0, // Remove Android shadow
-              shadowOpacity: 0, // Remove iOS shadow
-            },
-            tabBarLabelStyle: {
-              fontWeight: "bold",
-              fontSize: 12,
-              marginBottom: 10, // Adjust the space between the icon and the text
-            },
-          })}
-        >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="My Cards" component={MyCards} />
-          <Tab.Screen name="Statistics" component={Statistics} />
-          <Tab.Screen name="Settings" component={Settings} />
-        </Tab.Navigator>
+        <AppNavigator />
       </NavigationContainer>
     </ThemeProvider>
   );
