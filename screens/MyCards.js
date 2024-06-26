c5bb554592d445d7147f072e5d1a5e4f7ae1bdfb
bennydan7 <baowusu-dankwah@st.ug.edu.gh>
2024-06-26 14:01:18 +0000

    modified cards

--- screens/MyCards.js
+++ screens/MyCards.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { View, Text } from "react-native";
+import { ThemeContext } from "../ThemeContext";
+import { ThemeLight, ThemeDark } from "../components/appData";
 
 const MyCards = () => {
-  return <div>MyCards</div>;
+  const { theme } = useContext(ThemeContext);
+
+  const backgroundColor =
+    theme === "light" ? ThemeLight.backgroundColor : ThemeDark.backgroundColor;
+
+  return (
+    <View style={[styles.container, { backgroundColor }]}>
+      <Text>My Cards💳</Text>
+    </View>
+  );
+};
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+  },
 };
 
 export default MyCards;
